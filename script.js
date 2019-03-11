96b7d59aca10071be3f32fee56fadc263fe12d04
jfark <jessamyn.farkas@gmail.com>
2019-03-10 22:48:19 -0400

    donationAmount beginning prompt

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ var churchillSpeech = {
 
 document.getElementById('BtnDonate').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Donate" button.
+	var donationAmount = prompt("How much would you like to donate?");
+	
 });
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
